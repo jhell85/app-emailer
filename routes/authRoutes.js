1272ce79ecd906a250a9017b79a1881ad9476056
jhell85 <jhell85@gmail.com>
2020-12-13 13:17:12 -0800

    add production variables

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,5 +7,19 @@ module.exports = (app) => {
       scope: ["profile", "email"],
     })
   );
+
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    console.log(req.user);
+    if (!req.user) {
+      res.send({ user: null });
+    }
+    res.send(req.user);
+  });
 };
