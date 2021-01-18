6533fd5e29ae1f0dad35c402a73e6de126b25dc7
jhell85 <jhell85@gmail.com>
2021-01-18 10:47:11 -0800

    add render checks in header for loginand logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,11 +16,6 @@ module.exports = (app) => {
   });
 
   app.get("/api/current_user", (req, res) => {
-    console.log("blah blah /api/current_user", req.user);
-    if (!req.user) {
-      console.log("no user present");
-      res.send({ user: null });
-    }
     res.send(req.user);
   });
 };
